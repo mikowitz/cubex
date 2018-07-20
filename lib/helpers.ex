ba5522cf8848828e4852fa5a141994e6e64434f9
Michael Berkowitz <michael.berkowitz@gmail.com>
2018-07-19 23:20:52 -0400

    Refactor Solver.Cross

---
+++ lib/helpers.ex
@@ -0,0 +1,32 @@
+defmodule Cube.Helpers do
+  @sides ~w(f r b l)
+
+  def rotate([], _), do: []
+  def rotate(list, 0), do: list
+  def rotate(list, n) when n < 0 do
+    Enum.reverse(list) |> rotate(-n) |> Enum.reverse
+  end
+  def rotate([h|t], n), do: rotate(t ++ [h], n - 1)
+
+  def make_moves({cube, moves}, new_moves, face) do
+    new_moves = create_step(new_moves, face)
+    {Cube2.turn(cube, new_moves), moves ++ [new_moves]}
+  end
+
+  def face_names(front), do: @sides |> rotate(face_index(front))
+
+  def face_index(face) do
+    Enum.find_index(@sides, &Kernel.==(&1,face))
+  end
+
+  def create_step(steps, face) do
+    face_map = Enum.zip(@sides, face_names(face))
+    String.split(steps) |> Enum.map(fn step ->
+      case Enum.find(face_map, fn {f, _} -> String.contains?(step, f) end) do
+        nil -> step
+        {same, same} -> step
+        {old, new} -> String.replace(step, old, new)
+      end
+    end) |> Enum.join(" ")
+  end
+end
--- lib/solver/cross.ex
+++ lib/solver/cross.ex
@@ -1,234 +1,120 @@
 defmodule Solver.Cross do
-  @sides ~w(f r b l)
+  @side_names ~w(front right back left)
+  @sides Enum.map(@side_names, &String.first/1)
+
+  import Cube.Helpers
 
   def solve({[_, _, _, _, "DF", "DR", "DB", "DL"|_], _} = set), do: set
-
-  # pos 0 up front
-  def solve({["DF"|_], _} = set), do: rotate_down(set, "f") |> solve
-  def solve({["FD"|_], _} = set), do: flip_down(set, "f") |> solve
-  def solve({["DR"|_], _} = set), do: rotate_down_right(set, "f") |> solve
-  def solve({["RD"|_], _} = set), do: flip_down_right(set, "f") |> solve
-  def solve({["DB"|_], _} = set), do: rotate_down_across(set, "f") |> solve
-  def solve({["BD"|_], _} = set), do: flip_down_across(set, "f") |> solve
-  def solve({["DL"|_], _} = set), do: rotate_down_left(set, "f") |> solve
-  def solve({["LD"|_], _} = set), do: flip_down_left(set, "f") |> solve
-
-  # pos 1 up right
-  def solve({[_,"DF"|_], _} = set), do: solve(rotate_down_left(set, "r"))
-  def solve({[_,"FD"|_], _} = set), do: solve(flip_down_left(set, "r"))
-  def solve({[_,"DR"|_], _} = set), do: solve(rotate_down(set, "r"))
-  def solve({[_,"RD"|_], _} = set), do: solve(flip_down(set, "r"))
-  def solve({[_,"DB"|_], _} = set), do: solve(rotate_down_right(set, "r"))
-  def solve({[_,"BD"|_], _} = set), do: solve(flip_down_right(set, "r"))
-  def solve({[_,"DL"|_], _} = set), do: solve(rotate_down_across(set, "r"))
-  def solve({[_,"LD"|_], _} = set), do: solve(flip_down_across(set, "r"))
-
-  # pos 2 up back
-  def solve({[_,_,"DF"|_], _} = set), do: solve(rotate_down_across(set, "b"))
-  def solve({[_,_,"FD"|_], _} = set), do: solve(flip_down_across(set, "b"))
-  def solve({[_,_,"DR"|_], _} = set), do: solve(rotate_down_left(set, "b"))
-  def solve({[_,_,"RD"|_], _} = set), do: solve(flip_down_left(set, "b"))
-  def solve({[_,_,"DB"|_], _} = set), do: solve(rotate_down(set, "b"))
-  def solve({[_,_,"BD"|_], _} = set), do: solve(flip_down(set, "b"))
-  def solve({[_,_,"DL"|_], _} = set), do: solve(rotate_down_right(set, "b"))
-  def solve({[_,_,"LD"|_], _} = set), do: solve(flip_down_right(set, "b"))
-
-  # pos 3 up left
-  def solve({[_,_,_,"DF"|_], _} = set), do: solve(rotate_down_right(set, "l"))
-  def solve({[_,_,_,"FD"|_], _} = set), do: solve(flip_down_right(set, "l"))
-  def solve({[_,_,_,"DR"|_], _} = set), do: solve(rotate_down_across(set, "l"))
-  def solve({[_,_,_,"RD"|_], _} = set), do: solve(flip_down_across(set, "l"))
-  def solve({[_,_,_,"DB"|_], _} = set), do: solve(rotate_down_left(set, "l"))
-  def solve({[_,_,_,"BD"|_], _} = set), do: solve(flip_down_left(set, "l"))
-  def solve({[_,_,_,"DL"|_], _} = set), do: solve(rotate_down(set, "l"))
-  def solve({[_,_,_,"LD"|_], _} = set), do: solve(flip_down(set, "l"))
-
-  # pos 4
-  # def solve({[_,_,_,_,"DF"|_], _} = set), do: solve(set)
-  def solve({[_,_,_,_,"FD"|_], _} = set), do: solve(flip(set, "f"))
-  def solve({[_,_,_,_,"DR"|_], _} = set), do: solve(rotate_right(set, "f"))
-  def solve({[_,_,_,_,"RD"|_], _} = set), do: solve(flip_right(set, "f"))
-  def solve({[_,_,_,_,"DB"|_], _} = set), do: solve(rotate_across(set, "f"))
-  def solve({[_,_,_,_,"BD"|_], _} = set), do: solve(flip_across(set, "f"))
-  def solve({[_,_,_,_,"DL"|_], _} = set), do: solve(rotate_left(set, "f"))
-  def solve({[_,_,_,_,"LD"|_], _} = set), do: solve(flip_left(set, "f"))
-
-  # pos 5
-  def solve({[_,_,_,_,_,"DF"|_], _} = set), do: solve(rotate_left(set, "r"))
-  def solve({[_,_,_,_,_,"FD"|_], _} = set), do: solve(flip_left(set, "r"))
-  # def solve({[_,_,_,_,_,"DR"|_], _} = set), do: solve(set)
-  def solve({[_,_,_,_,_,"RD"|_], _} = set), do: solve(flip(set, "r"))
-  def solve({[_,_,_,_,_,"DB"|_], _} = set), do: solve(rotate_right(set, "r"))
-  def solve({[_,_,_,_,_,"BD"|_], _} = set), do: solve(flip_right(set, "r"))
-  def solve({[_,_,_,_,_,"DL"|_], _} = set), do: solve(rotate_across(set, "r"))
-  def solve({[_,_,_,_,_,"LD"|_], _} = set), do: solve(flip_across(set, "r"))
-
-  # pos 6
-  def solve({[_,_,_,_,_,_,"DF"|_], _} = set), do: solve(rotate_across(set, "b"))
-  def solve({[_,_,_,_,_,_,"FD"|_], _} = set), do: solve(flip_across(set, "b"))
-  def solve({[_,_,_,_,_,_,"DR"|_], _} = set), do: solve(rotate_left(set, "b"))
-  def solve({[_,_,_,_,_,_,"RD"|_], _} = set), do: solve(flip_left(set, "b"))
-  # def solve({[_,_,_,_,_,_,"DB"|_], _} = set), do: solve(set)
-  def solve({[_,_,_,_,_,_,"BD"|_], _} = set), do: solve(flip(set, "b"))
-  def solve({[_,_,_,_,_,_,"DL"|_], _} = set), do: solve(rotate_right(set, "b"))
-  def solve({[_,_,_,_,_,_,"LD"|_], _} = set), do: solve(flip_right(set, "b"))
-
-  # pos 7
-  def solve({[_,_,_,_,_,_,_,"DF"|_], _} = set), do: solve(rotate_right(set, "l"))
-  def solve({[_,_,_,_,_,_,_,"FD"|_], _} = set), do: solve(flip_right(set, "l"))
-  def solve({[_,_,_,_,_,_,_,"DR"|_], _} = set), do: solve(rotate_across(set, "l"))
-  def solve({[_,_,_,_,_,_,_,"RD"|_], _} = set), do: solve(flip_across(set, "l"))
-  def solve({[_,_,_,_,_,_,_,"DB"|_], _} = set), do: solve(rotate_left(set, "l"))
-  def solve({[_,_,_,_,_,_,_,"BD"|_], _} = set), do: solve(flip_left(set, "l"))
-  # def solve({[_,_,_,_,_,_,_,"DL"|_], _} = set), do: solve(set)
-  def solve({[_,_,_,_,_,_,_,"LD"|_], _} = set), do: solve(flip(set, "l"))
-
-  # pos 8 front right
-  def solve({[_,_,_,_,_,_,_,_,"DF"|_], _} = set), do: solve(middle_front_to_front(set, "f"))
-  def solve({[_,_,_,_,_,_,_,_,"FD"|_], _} = set), do: solve(middle_right_to_front(set, "f"))
-  def solve({[_,_,_,_,_,_,_,_,"DR"|_], _} = set), do: solve(middle_front_to_right(set, "f"))
-  def solve({[_,_,_,_,_,_,_,_,"RD"|_], _} = set), do: solve(middle_right_to_right(set, "f"))
-  def solve({[_,_,_,_,_,_,_,_,"DB"|_], _} = set), do: solve(middle_front_to_back(set, "f"))
-  def solve({[_,_,_,_,_,_,_,_,"BD"|_], _} = set), do: solve(middle_right_to_back(set, "f"))
-  def solve({[_,_,_,_,_,_,_,_,"DL"|_], _} = set), do: solve(middle_front_to_left(set, "f"))
-  def solve({[_,_,_,_,_,_,_,_,"LD"|_], _} = set), do: solve(middle_right_to_left(set, "f"))
-
-  # pos 9 front left
-  def solve({[_,_,_,_,_,_,_,_,_,"DF"|_], _} = set), do: solve(middle_right_to_right(set, "l"))
-  def solve({[_,_,_,_,_,_,_,_,_,"FD"|_], _} = set), do: solve(middle_front_to_right(set, "l"))
-  def solve({[_,_,_,_,_,_,_,_,_,"DR"|_], _} = set), do: solve(middle_right_to_back(set, "l"))
-  def solve({[_,_,_,_,_,_,_,_,_,"RD"|_], _} = set), do: solve(middle_front_to_back(set, "l"))
-  def solve({[_,_,_,_,_,_,_,_,_,"DB"|_], _} = set), do: solve(middle_right_to_left(set, "l"))
-  def solve({[_,_,_,_,_,_,_,_,_,"BD"|_], _} = set), do: solve(middle_front_to_left(set, "l"))
-  def solve({[_,_,_,_,_,_,_,_,_,"DL"|_], _} = set), do: solve(middle_right_to_front(set, "l"))
-  def solve({[_,_,_,_,_,_,_,_,_,"LD"|_], _} = set), do: solve(middle_front_to_front(set, "l"))
-
-  # pos 10 back right
-  def solve({[_,_,_,_,_,_,_,_,_,_,"DF"|_], _} = set), do: solve(middle_right_to_left(set, "r"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,"FD"|_], _} = set), do: solve(middle_front_to_left(set, "r"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,"DR"|_], _} = set), do: solve(middle_right_to_front(set, "r"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,"RD"|_], _} = set), do: solve(middle_front_to_front(set, "r"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,"DB"|_], _} = set), do: solve(middle_right_to_right(set, "r"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,"BD"|_], _} = set), do: solve(middle_front_to_right(set, "r"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,"DL"|_], _} = set), do: solve(middle_right_to_back(set, "r"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,"LD"|_], _} = set), do: solve(middle_front_to_back(set, "r"))
-
-  # pos 11 back left
-  def solve({[_,_,_,_,_,_,_,_,_,_,_,"DF"|_], _} = set), do: solve(middle_front_to_back(set, "b"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,_,"FD"|_], _} = set), do: solve(middle_right_to_back(set, "b"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,_,"DR"|_], _} = set), do: solve(middle_front_to_left(set, "b"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,_,"RD"|_], _} = set), do: solve(middle_right_to_left(set, "b"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,_,"DB"|_], _} = set), do: solve(middle_front_to_front(set, "b"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,_,"BD"|_], _} = set), do: solve(middle_right_to_front(set, "b"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,_,"DL"|_], _} = set), do: solve(middle_front_to_right(set, "b"))
-  def solve({[_,_,_,_,_,_,_,_,_,_,_,"LD"|_], _} = set), do: solve(middle_right_to_right(set, "b"))
-
-  def middle_front_to_front(set, face) do
-    [_, r, _, _] = face_names(face)
-    make_moves(set, "d #{r}' d'")
-  end
-  def middle_right_to_front(set, face) do
-    [f, _, _, _] = face_names(face)
-    make_moves(set, "#{f}")
-  end
-  def middle_front_to_right(set, face) do
-    [_, r, _, _] = face_names(face)
-    make_moves(set, "#{r}'")
-  end
-  def middle_right_to_right(set, face) do
-    [f, _, _, _] = face_names(face)
-    make_moves(set, "d' #{f} d")
-  end
-  def middle_front_to_back(set, face) do
-    [_, r, _, _] = face_names(face)
-    make_moves(set, "d' #{r}' d")
-  end
-  def middle_right_to_back(set, face) do
-    [f, _, _, _] = face_names(face)
-    make_moves(set, "d2 #{f} d2")
-  end
-  def middle_front_to_left(set, face) do
-    [_, r, _, _] = face_names(face)
-    make_moves(set, "d2 #{r}' d2")
-  end
-  def middle_right_to_left(set, face) do
-    [f, _, _, _] = face_names(face)
-    make_moves(set, "d #{f} d'")
+  def solve({cube, _} = set) do
+    {cubie, index} = find_first_unplaced_cross_cubie(cube)
+    {func, face} = lookup_function_and_face(cubie, index)
+    apply(__MODULE__, func, [set, face]) |> solve
   end
 
-  def flip(set, face) do
-    [f, r, _, _] = face_names(face)
-    make_moves(set, "#{f}' d #{r}' d'")
-  end
-  def rotate_right(set, face) do
-    [f, r, _, _] = face_names(face)
-    make_moves(set, "#{f}2 u' #{r}2")
-  end
-  def flip_right(set, face) do
-    [f, r, _, _] = face_names(face)
-    make_moves(set, "#{f}' #{r}'")
-  end
-  def rotate_across(set, face) do
-    [f, _, b, _] = face_names(face)
-    make_moves(set, "#{f}2 u2 #{b}2")
-  end
-  def flip_across(set, face) do
-    [_, r, b, _] = face_names(face)
-    make_moves(set, "d #{r}' d' #{b}'")
-  end
-  def rotate_left(set, face) do
-    [f, _, _, l] = face_names(face)
-    make_moves(set, "#{f}2 u #{l}2")
-  end
-  def flip_left(set, face) do
-    [f, _, _, l] = face_names(face)
-    make_moves(set, "#{f} #{l}")
+  def rotate_down(set, face), do: make_moves(set, "f2", face)
+  def rotate_down_right(set, face), do: make_moves(set, "u' r2", face)
+  def rotate_down_across(set, face), do: make_moves(set, "u2 b2", face)
+  def rotate_down_left(set, face), do: make_moves(set, "u l2", face)
+
+  def flip_down(set, face), do: make_moves(set, "u' r' f r", face)
+  def flip_down_right(set, face), do: make_moves(set, "f r' f'", face)
+  def flip_down_across(set, face), do: make_moves(set, "u' r b' r'", face)
+  def flip_down_left(set, face), do: make_moves(set, "f' l f", face)
+
+  def rotate_right(set, face), do: make_moves(set, "f2 u' r2", face)
+  def rotate_across(set, face), do: make_moves(set, "f2 u2 b2", face)
+  def rotate_left(set, face), do: make_moves(set, "f2 u l2", face)
+
+  def flip(set, face), do: make_moves(set, "f' d r' d'", face)
+  def flip_right(set, face), do: make_moves(set, "f' r'", face)
+  def flip_across(set, face), do: make_moves(set, "d r' d' b'", face)
+  def flip_left(set, face), do: make_moves(set, "f l", face)
+
+  def middle_front_to_front(set, face), do: make_moves(set, "d r' d'", face)
+  def middle_front_to_right(set, face), do: make_moves(set, "r'", face)
+  def middle_front_to_back(set, face), do: make_moves(set, "d' r' d", face)
+  def middle_front_to_left(set, face), do: make_moves(set, "d2 r' d2", face)
+
+  def middle_right_to_front(set, face), do: make_moves(set, "f", face)
+  def middle_right_to_right(set, face), do: make_moves(set, "d' f d", face)
+  def middle_right_to_back(set, face), do: make_moves(set, "d2 f d2", face)
+  def middle_right_to_left(set, face), do: make_moves(set, "d f d'", face)
+
+  def find_first_unplaced_cross_cubie(cube) do
+    Enum.with_index(cube) |> Enum.find(fn {cubie, index} ->
+      String.length(cubie) == 2 &&
+        Regex.match?(~r/D/, cubie) &&
+          !correctly_placed?(cubie, index)
+    end)
   end
 
-  def rotate_down(set, face) do
-    make_moves(set, "#{face}2")
-  end
-  def flip_down(set, face) do
-    [f, r, _, _] = face_names(face)
-    make_moves(set, "u' #{r}' #{f} #{r}")
-  end
-  def rotate_down_across(set, face) do
-    [_, _, b, _] = face_names(face)
-    make_moves(set, "u2 #{b}2")
-  end
-  def flip_down_across(set, face) do
-    [_, r, b, _] = face_names(face)
-    make_moves(set, "u' #{r} #{b}' #{r}'")
+  def correctly_placed?("DF", 4), do: true
+  def correctly_placed?("DR", 5), do: true
+  def correctly_placed?("DB", 6), do: true
+  def correctly_placed?("DL", 7), do: true
+  def correctly_placed?(_, _), do: false
+
+  def lookup_function_and_face(cubie, index) do
+    lookup_table()[cubie] |> Enum.at(index)
   end
 
-  def rotate_down_right(set, face) do
-    [_, r, _, _] = face_names(face)
-    make_moves(set, "u' #{r}2")
-  end
-  def flip_down_right(set, face) do
-    [f, r, _, _] = face_names(face)
-    make_moves(set, "#{f} #{r}' #{f}'")
-  end
-
-  def rotate_down_left(set, face) do
-    [_, _, _, l] = face_names(face)
-    make_moves(set, "u #{l}2")
-  end
-  def flip_down_left(set, face) do
-    [f, _, _, l] = face_names(face)
-    make_moves(set, "#{f}' #{l} #{f}")
+  defp lookup_table do
+    Enum.map(@sides, fn face ->
+      uppercase_face = String.upcase(face)
+      [
+        {"D#{uppercase_face}", build_turn_map(:rotation, face)},
+        {"#{uppercase_face}D", build_turn_map(:flip, face)}
+      ]
+    end) |> Enum.concat |> Enum.into(%{})
   end
 
-  def face_names(front) do
-    @sides |> Stream.cycle
-    |> Stream.drop(Enum.find_index(@sides, &Kernel.==(&1,front)))
-    |> Enum.take(4)
+  def build_turn_map(type, face) do
+    [:top, :bottom, :middle] |> Enum.map(fn prefixix ->
+      apply(__MODULE__, :"#{prefixix}_layer_#{type}_fields", [face])
+    end) |> Enum.concat
   end
 
-  defp make_moves({cube, moves}, new_moves) do
-    {
-      Cube2.turn(cube, new_moves),
-      moves ++ [new_moves]
-    }
+  def top_layer_rotation_fields(face) do
+    top_or_bottom_layer_function_names_for(face, fn {suffix, f} -> {:"rotate_down#{suffix}", f} end)
+  end
+
+  def middle_layer_rotation_fields(face) do
+    middle_layer_function_names_for(face, [:front, :right], fn {prefix, suffix, f} ->
+      {:"middle_#{prefix}_to_#{Enum.at(@side_names, face_index(suffix))}", f}
+    end)
+  end
+
+  def bottom_layer_rotation_fields(face) do
+    top_or_bottom_layer_function_names_for(face, fn
+      {"", _} -> nil
+      {suffix, face} -> {:"rotate#{suffix}", face}
+    end)
+  end
+
+  def top_layer_flip_fields(face) do
+    top_or_bottom_layer_function_names_for(face, fn {suffix, f} -> {:"flip_down#{suffix}", f} end)
+  end
+
+  def middle_layer_flip_fields(face) do
+    middle_layer_function_names_for(face, [:right, :front], fn {prefix, suffix, f} ->
+      {:"middle_#{prefix}_to_#{Enum.at(@side_names, face_index(suffix))}", f}
+    end)
+  end
+
+  def bottom_layer_flip_fields(face) do
+    top_or_bottom_layer_function_names_for(face, fn {suffix, f} -> {:"flip#{suffix}", f} end)
+  end
+
+  def top_or_bottom_layer_function_names_for(face, func) do
+    ["", "_left", "_across", "_right"] |> rotate(-face_index(face))
+    |> Enum.zip(@sides) |> Enum.map(&func.(&1))
+  end
+
+  def middle_layer_function_names_for(face, [prefix1, prefix2], func) do
+    [f, r, b, l] = face_names(face)
+    Enum.zip([
+      [prefix1, prefix2, prefix2, prefix1],
+      [f, r, l, b], ~w(f l r b)
+    ]) |> Enum.map(&func.(&1))
   end
 end
